c98fc8b1fb100af681c34cc31185daebed0265c2
Oleksandr <gayrsuk@gmail.com>
2025-03-22 21:18:51 +0200

    refactored pj structure

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from '../../models/auth/auth.service';
 import { PrismaModule } from '../../../prisma/prisma.module';
-import { UsersService } from '../../models/users/users.service';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [PrismaModule],
--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,12 +1,19 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsEmail, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
-  username: string | undefined;
+  @IsNotEmpty({message: 'Username is required'})
+    // @ts-ignore
+  username: string;
+
+  @IsEmail({}, {message: 'Email must be a valid email address'})
+  @IsNotEmpty({message: 'Email is required'})
+    // @ts-ignore
+  email: string;
 
   @IsString()
-  email: string | undefined;
-
-  @IsString()
-  passwordHash: string | undefined;
+  @MinLength(6, {message: 'Password must be at least 6 characters long'})
+  @IsNotEmpty({message: 'Password is required'})
+    // @ts-ignore
+  passwordHash: string;
 }
