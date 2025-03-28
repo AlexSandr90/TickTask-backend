3a6ab22d18af3ff1d99fb4e8fdfaf46ebefa15eb
CodeByArtem <smailxxxvizde@gmail.com>
2025-03-28 19:56:56 +0100

    cors

--- src/configurations/cors.config.ts
+++ src/configurations/cors.config.ts
@@ -1,14 +1,14 @@
-import { INestApplication } from "@nestjs/common";
-
-export function configureCors(app: INestApplication) {
-  app.enableCors({
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? ['https://your-frontend-app.com']
-        : ['http://localhost:3000', 'http://localhost:4200'],
-
-    methods: 'GET,POST,PUT,DELETE, PATCH, OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization, Cookie',
-    credentials: true,
-  });
-}
+// import { INestApplication } from "@nestjs/common";
+//
+// export function configureCors(app: INestApplication) {
+//   app.enableCors({
+//     origin:
+//       process.env.NODE_ENV === 'production'
+//         ? ['https://your-frontend-app.com']
+//         : ['http://localhost:3000', 'http://localhost:4200'],
+//
+//     methods: 'GET,POST,PUT,DELETE, PATCH, OPTIONS',
+//     allowedHeaders: 'Content-Type, Authorization, Cookie',
+//     credentials: true,
+//   });
+// }
