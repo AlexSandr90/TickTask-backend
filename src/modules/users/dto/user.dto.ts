28c2d480a45f9a42b74cd2c6881c993a590269f9
Oleksandr <gayrsuk@gmail.com>
2025-04-27 13:46:15 +0300

    Update user.dto.ts

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -2,27 +2,42 @@ import { IsOptional, IsString, IsBoolean, IsEmail } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateUserDto {
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'User name',
+    example: 'Nikolas',
+  })
   @IsOptional()
   @IsString()
   username?: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'User Email',
+    example: 'nikolas.kage@gmail.com',
+  })
   @IsOptional()
   @IsEmail()
   email?: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'User theme',
+    example: 'light',
+  })
   @IsOptional()
   @IsString()
   theme?: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'User notification',
+    example: false,
+  })
   @IsOptional()
   @IsBoolean()
   notifications?: boolean;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'Is user activated his account',
+    example: false,
+  })
   @IsOptional()
   @IsBoolean()
   isActive?: boolean;
