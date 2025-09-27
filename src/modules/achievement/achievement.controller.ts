4e50bb70ffc3a815e05fbe5a248824abef87dba7
CodeByArtem <150847319+CodeByArtem@users.noreply.github.com>
2025-09-27 22:27:47 +0200

    Update achievement.controller.ts

--- src/modules/achievement/achievement.controller.ts
+++ src/modules/achievement/achievement.controller.ts
@@ -60,7 +60,7 @@ export class AchievementController {
 
   @Post('initialize')
   @JwtAuthDecorator()
-  @ApiOperation({ summary: 'Initialize basic achievement' })
+  @ApiOperation({ summary: 'Initialize basic achievements' })
   async initializeBasics(): Promise<InitializeAchievementsResponseDto> {
     return this.achievementService.initializeBasicAchievements();
   }
