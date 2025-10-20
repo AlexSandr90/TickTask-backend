5f630ae31efd21ccc9254d455056c46f91c522f1
Oleksandr <gayrsuk@gmail.com>
2025-10-21 00:01:33 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api-docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
